9127e0f4e6bd1744d73e265e63627b0ee4f4500c
Ripon <alamin.ripon@brainstation-23.com>
2025-07-04 18:30:47 +0600

    What?  - invalid uuid error handled

--- student_manager/utils/helper_methods.py
+++ student_manager/utils/helper_methods.py
@@ -4,6 +4,6 @@ from .custom_global_exceptions import InvalidUUIDException
 
 def validate_uuid(uuid_string):
     try:
-        return uuid.UUID(uuid_string)
-    except (ValueError, TypeError):
+        return uuid.UUID(str(uuid_string))
+    except ValueError:
         raise InvalidUUIDException
