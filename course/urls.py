d46590f7c9ad1703dfe20efe1dcf0dd02c341708
Ripon <alamin.ripon@brainstation-23.com>
2025-07-04 15:51:57 +0600

    What?  - url updated

--- course/urls.py
+++ course/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CourseListCreateAPIView
 
 urlpatterns = [
-    path('courses/', CourseListCreateAPIView.as_view(), name="course_list_create"),
+    path('api/courses/', CourseListCreateAPIView.as_view(), name="course_list_create"),
     # path("courses/<int:pk>/", CourseRetrieveUpdateDestroyAPIView.as_view(), name="course_detail"),
     # path("students/", StudentListCreateAPIView.as_view(), name="student_list_create"),
     # path("students/<int:pk>/", StudentRetrieveUpdateDestroyAPIView.as_view(), name="student_detail"),
