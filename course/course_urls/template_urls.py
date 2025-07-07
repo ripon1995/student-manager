7e42069bf456e882d08220dc8762515fdb9b4210
Ripon <alamin.ripon@brainstation-23.com>
2025-07-07 13:20:07 +0600

    What?  - course detail page loading

--- course/course_urls/template_urls.py
+++ course/course_urls/template_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from course.views.template_views import CourseListView
+from course.views.template_views import CourseListView, CourseDetailView
 
 urlpatterns = [
     path('courses/', CourseListView.as_view(), name='course_list_view'),
+    path('course/<pk>/', CourseDetailView.as_view(), name='course_detail_view'),
 ]
