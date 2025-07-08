1cd0f48e041591b342e62bbe5a6686299a4f42ae
Ripon <alamin.ripon@brainstation-23.com>
2025-07-08 14:44:16 +0600

    What?  - super user for production

--- student_manager/management/commands/create_initial_super_user.py
+++ student_manager/management/commands/create_initial_super_user.py
@@ -7,9 +7,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         User = get_user_model()
-        username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
-        email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-        password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+        username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+        email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+        password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
