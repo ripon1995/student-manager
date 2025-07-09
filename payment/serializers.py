826e9c78d3ac548d3a2ce52127b6509da2b02c22
Ripon <alamin.ripon@brainstation-23.com>
2025-07-09 17:31:54 +0600

    What? - payment list api for current month

--- payment/serializers.py
+++ payment/serializers.py
@@ -6,7 +6,11 @@ class PaymentListCreateSerializer(ModelSerializer):
         model = Payment
         fields = ['id', 'amount', 'payment_for_month', 'date', 'student', 'course']
         read_only_fields = ['date']  # Date should be set automatically on creation
-        extra_kwargs = {
-            'student': {'required': True},
-            'course': {'required': True}
-        }
+
+
+
+class CurrentMonthPaymentListSerializer(ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = ['id', 'amount', 'payment_for_month', 'date', 'student', 'course']
+        read_only_fields = ['date']  # Date should be set automatically on creation
